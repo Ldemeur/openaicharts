d6489bd4aa0a5c5a4f391eb93fa8816dd37c3fd1
Fela <217143+fela@users.noreply.github.com>
2023-03-30 22:07:36 +0200

    working version

--- auto_scientist.py
+++ auto_scientist.py
@@ -1,18 +1,25 @@
+import io
 import json
+import sys
 from typing import List, Dict
+
+import os
+import hashlib
+import shelve
+import pickle
 
 import openai
 
-PROMPT = "You are a creative rockstar data scientist. Your task is to analyse a data source, this is a file" + \
-    "Called food-enforcement.json you can execute python commands only. For example start by printing" + \
-    "A few lines of the file, then read them, and start running code as you choose.\n" + \
-    "Each of your outputs should be in strictly in json format. Your json should have two keys: " + \
+PROMPT = "You are a creative rockstar data scientist. Your task is to analyse a data source, this is a file" \
+    "Called food-enforcement.json you can execute python commands only. For example start by printing" \
+    "A few lines of the file, then read them, and start running code as you choose.\n" \
+    "Each of your outputs should be in strictly in json format. Your json should have two keys: " \
     "'comment': Should be a pretty short comment about what you are doing or what you are finding out. " \
     "Remember to keep it short even feel free to leave it empty if you are nothing interesting to say." \
-    "'python' or 'shell': Should be a string containing a valid python or shell code including imports if needed" + \
-    "max 1000 characters of the results will be returned to you, might be an error in which case you should correct" + \
-    "Yourself by writing better code, when that happens. Don't do too much after each request. Remember to only write json" + \
-    "So start with { ... etc"
+    "'python' or 'shell': Should be a string containing a valid python or shell code including imports if needed" \
+    "max 1000 characters of the results will be returned to you, might be an error in which case you should correct" \
+    "Yourself by writing better code, when that happens. Don't do too much after each request. Remember to only write" \
+    " json. S start with { ... etc. Do use print statements, as we do not print your last commands like in notebooks"
 
 
 openai.api_key = 'sk-...'
@@ -20,10 +27,6 @@ openai.api_key = 'sk-...'
 
 Prompt = List[Dict[str, str]]
 
-import os
-import hashlib
-import shelve
-import pickle
 
 # The directory where the cache files will be stored
 # This should be set to a directory that is not in the project directory
@@ -69,42 +72,67 @@ def chat_api(messages: Prompt, model: str = 'gpt-4', seed: int = 0, **kwargs):
 def main():
     messages = [{'role': 'system', 'content': PROMPT}]
     while True:
-        res = chat_api(messages, seed=1)
+        res = chat_api(messages, seed=0)
         res_str = res.choices[0]["message"]["content"]
+        messages.append({'role': 'assistant', 'content': str(res_str)})
+        print("# Assistant:")
+        print(res_str)
         try:
             res_data = json.loads(res_str)
-        except:
-            messages.append({'role': 'assistant', 'content': str(res_str)})
-            messages.append({'role': 'system', 'content': 'invalid json'})
+        except Exception as ex:
+            msg = f'Please always only respond with a valid json (${str(ex)})'
+            print("# System:")
+            print(msg)
+            messages.append({'role': 'system', 'content': f'Please always only respond with a valid json (${str(ex)})'})
             continue
 
         if 'comment' in res_data:
+            print("## Extracting comment:")
             print(res_data['comment'])
         if 'python' in res_data:
+            print("## Extracting python code:")
             print(res_data['python'])
             # ask user if he wants to execute
-            print('Do you want to execute this code? (y/n)')
+            print('## Do you want to execute this code? (y/n)')
             if input() == 'y':
                 # execute and save std out result in out
                 try:
-                    out = exec(res_data['python'])
+                    # Save the current stdout and stderr
+                    original_stdout = sys.stdout
+                    original_stderr = sys.stderr
+
+                    # Create a StringIO buffer to store the output and error
+                    captured_stdout = io.StringIO()
+                    captured_stderr = io.StringIO()
+
+                    # Redirect stdout and stderr to the buffer
+                    sys.stdout = captured_stdout
+                    sys.stderr = captured_stderr
+                    exec(res_data['python'])
+                    out = ''
                 except Exception as ex:
-                    out = str(ex)
-                messages.append({'role': 'assistant', 'content': str(res_str)})
+                    out = f"Got exception: ${ex}"
+                finally:
+                    # Restore the original stdout and stderr
+                    sys.stdout = original_stdout
+                    sys.stderr = original_stderr
+
+                    # Get the output and error as strings
+                    output = captured_stdout.getvalue()
+                    error = captured_stderr.getvalue()
+
+                    # Close the StringIO buffer
+                    stdout = captured_stdout.getvalue()
+                    stderr = captured_stderr.getvalue()
+                    captured_stdout.close()
+                    captured_stderr.close()
+                if stdout:
+                    out += f'\n\noutput:\n{stdout}'
+                if stderr:
+                    out += f'\n\nerror:\n{stderr}'
+                print("# User:")
+                print(out)
                 messages.append({'role': 'user', 'content': out})
-        print(res)
-
-
-CODE = '''
-import json
-
-with open('food-enforcement.json') as file:
-    data_lines = [next(file) for x in range(5)]
-print(json.dumps(data_lines))
-'''
-
 
 if __name__ == '__main__':
-#     out = exec(CODE)
-#     print(out)
-    main()
+   main()
